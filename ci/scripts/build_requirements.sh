5d0d58abc28a03983e3e3f14ece94f376025ac6c
Hamin Mousavi <hamin.mousavi@nordcloud.se>
2018-11-15 16:39:21 +0100

    pip problems

--- ci/scripts/build_requirements.sh
+++ ci/scripts/build_requirements.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get install -y python-dev jq bsdmainutils dnsutils envsubst
-curl -O https://bootstrap.pypa.io/get-pip.py | python get-pip.py
-pip install awscli
+curl -O https://bootstrap.pypa.io/get-pip.py | python get-pip.py |pip install awscli
