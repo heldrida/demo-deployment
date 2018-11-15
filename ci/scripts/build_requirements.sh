99a4b40ad5a7b12873bac3b4819dcee4c4bb49e2
Hamin Mousavi <hamin.mousavi@nordcloud.se>
2018-11-15 16:44:55 +0100

    pip again

--- ci/scripts/build_requirements.sh
+++ ci/scripts/build_requirements.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install -y python-dev jq bsdmainutils dnsutils envsubst
-curl -O https://bootstrap.pypa.io/get-pip.py | python get-pip.py |pip install awscli
+apt-get install -y python-dev python-pip jq bsdmainutils dnsutils envsubst
+pip install awscli
