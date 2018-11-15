2191757155bb1d931f9a7cba1de699d710996174
Hamin Mousavi <hamin.mousavi@nordcloud.se>
2018-11-15 16:36:00 +0100

    use envsubst to get environment variables into the docker container https://stackoverflow.com/questions/19537645/get-environment-variable-value-in-dockerfile

--- ci/scripts/build_requirements.sh
+++ ci/scripts/build_requirements.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install -y python-dev jq bsdmainutils dnsutils
+apt-get install -y python-dev jq bsdmainutils dnsutils envsubst
 curl -O https://bootstrap.pypa.io/get-pip.py | python get-pip.py
 pip install awscli
