e4b4ddc3115eed89bf8d1114e6e099ceaaad4926
Hamin Mousavi <hamin.mousavi@nordcloud.se>
2018-11-15 16:52:37 +0100

    last try

--- ci/scripts/build_requirements.sh
+++ ci/scripts/build_requirements.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 apt-get update
 apt-get install -y python-dev python-pip jq bsdmainutils dnsutils gettext-base
-pip install --upgrade pip
-pip install awscli
+python -m pip install awscli
