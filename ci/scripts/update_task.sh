fc73ef657e7c5bfd53d12bbfcdb4ff0690591a98
Hamin Mousavi <hamin.mousavi@nordcloud.se>
2018-11-22 12:37:21 +0100

    executionrolearn for task definition deployment

--- ci/scripts/update_task.sh
+++ ci/scripts/update_task.sh
@@ -19,7 +19,7 @@ make_task_def(){
 }
 make_task_def
 echo "Deploying from ci/task-definitions/$TASK_NAME-$BITBUCKET_COMMIT.json"
-rev=$(aws ecs register-task-definition --cli-input-json file://ci/task-definitions/$TASK_NAME-$BITBUCKET_COMMIT.json |  jq '.taskDefinition.revision')
+rev=$(aws ecs register-task-definition --execution-role-arn arn:aws:iam::145601632047:role/dev-cluster-EcsClusterRole-16LL2QW9XUCYK --cli-input-json file://ci/task-definitions/$TASK_NAME-$BITBUCKET_COMMIT.json |  jq '.taskDefinition.revision')
 echo "New revision $TASK_NAME-$rev created"
 
 # Get the ARN of the taskdefinition
